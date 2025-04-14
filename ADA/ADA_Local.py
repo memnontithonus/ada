ddf91c8d7944a3567c2685549eef436d17c004e5
Nazir Louis <nazirlouis38@gmail.com>
2025-04-14 17:42:12 -0400

    secruity

--- ADA/ADA_Local.py
+++ ADA/ADA_Local.py
@@ -19,13 +19,18 @@ class ADA:
         self.response_queue = asyncio.Queue()
         self.audio_queue = asyncio.Queue()
         self.recorder_config = {
+            'model': 'large-v3',
             'spinner': False,
             'language': 'en',
-            #'silero_sensitivity': 0.01,
-            #'webrtc_sensitivity': 3,
-            'post_speech_silence_duration': 0.01,
+            'silero_sensitivity': 0.01,
+            'webrtc_sensitivity': 3,
+            'post_speech_silence_duration': 0.1,
             'min_length_of_recording': 0.2,
             'min_gap_between_recordings': 0,
+
+            #'realtime_model_type': 'tiny.en',
+            #'enable_realtime_transcription': True,
+            #'on_realtime_transcription_update': self.clear_queues,
         }
 
         try:
